ba5b9116700974d0a38e4ce8f3da3f5f083a84ef
Svetlana <bel-svetik@yandex.ru>
2024-05-03 23:24:31 +0300

    Решить доп.задание без "for"

--- module1hard.py
+++ module1hard.py
@@ -1,13 +1,10 @@
 grades = ['5 3 3 5 4', '2 2 2 3', '4 5 5 2', '4 4 3', '5 5 5 4 5']
 students = {'Johnni', 'Bilbo', 'Steve', 'Kendrik', 'Aaron'}
 stud_list = list(sorted(students))
-#print(stud_list)
 student_dic = {}
-for i_stud in range(len(students)):
-    grade = list(map(int, (grades[i_stud].split())))
-    sum=0
-    for i_grad in range(len(grade)):
-        sum += grade[i_grad]
-    #print(i_stud, grade, sum/len(grade))
-    student_dic[stud_list[i_stud]] = sum/len(grade)
+
+grades_set = list(map(lambda x : list(map(int, x.split())), grades))
+#print(grades_set)
+
+student_dic = dict(map(lambda x, y : (x, sum(y)/len(y)), stud_list, grades_set))
 print(student_dic)
