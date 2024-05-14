8291d6878a6244e5e12a709f473e5d85fb00d6cc
Svetlana <bel-svetik@yandex.ru>
2024-05-14 23:57:39 +0300

    Решить доп.задание без "for" в одну строчку + zip

--- module1hard.py
+++ module1hard.py
@@ -6,5 +6,15 @@ students = {'Johnni', 'Bilbo', 'Steve', 'Kendrik', 'Aaron'}
 #grades_set = list(map(lambda x : list(map(int, x.split())), grades))
 #student_dic = dict(map(lambda x, y : (x, sum(y)/len(y)), stud_list, grades_set))
 
-student_dic = dict(map(lambda x, y : (x, sum(list(map(int, y.split())))/len(list(map(int, y.split())))), list(sorted(students)), grades))
+# ========== Вариант №1 ============
+
+student_dic = dict(map(lambda x, y : (x, sum(list(map(int, y.split())))/len(list(map(int, y.split())))),
+                       list(sorted(students)), grades))
 print(student_dic)
+
+# ========== Вариант №2 ============
+
+grades_set = list(map(lambda x : sum(list(map(int, x.split())))/len(list(map(int, x.split()))), grades))
+student_dic2 = dict(zip(list(sorted(students)), grades_set))
+
+print(student_dic2)
