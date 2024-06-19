d68722661b0ab7710bab6b7fdb958ae518d31841
Svetlana <bel-svetik@yandex.ru>
2024-06-19 22:58:54 +0300

    Решить доп.задание без "for" в одну строчку + zip

--- module1hard.py
+++ module1hard.py
@@ -1,3 +1,18 @@
+# Задание "Средний балл":
+# Вам необходимо решить задачу из реальной жизни: "школьные учителя устали подсчитывать
+# вручную средний балл каждого ученика, поэтому вам предстоит автоматизировать этот процесс":
+#
+# На вход даны следующие даннные:
+# Список: grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# Множество: students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+#
+# Список grades содержит списки оценок для каждого ученика в алфавитном порядке.
+# Например: 'Aaron' - [5, 3, 3, 5, 4]
+# Множество students содержит неупорядоченную последовательность имён всех учеников в классе.
+#
+# Напишите программу, которая составляет словарь, используя grades и students, где ключом
+# будет имя ученика, а значением - его средний балл.
+
 grades = ['5 3 3 5 4', '2 2 2 3', '4 5 5 2', '4 4 3', '5 5 5 4 5']
 students = {'Johnni', 'Bilbo', 'Steve', 'Kendrik', 'Aaron'}
 
@@ -8,13 +23,19 @@ students = {'Johnni', 'Bilbo', 'Steve', 'Kendrik', 'Aaron'}
 
 # ========== Вариант №1 ============
 
-student_dic = dict(map(lambda x, y : (x, sum(list(map(int, y.split())))/len(list(map(int, y.split())))),
+student_dic = dict(map(lambda x, y: (x, sum(list(map(int, y.split())))/len(list(map(int, y.split())))),
                        list(sorted(students)), grades))
 print(student_dic)
 
 # ========== Вариант №2 ============
 
-grades_set = list(map(lambda x : sum(list(map(int, x.split())))/len(list(map(int, x.split()))), grades))
+grades_set = list(map(lambda x: sum(list(map(int, x.split())))/len(list(map(int, x.split()))), grades))
 student_dic2 = dict(zip(list(sorted(students)), grades_set))
 
 print(student_dic2)
+
+# ========== Вариант №3 ============
+
+student_dic3 = dict(zip(list(sorted(students)), grades_set))
+
+print(student_dic3)
